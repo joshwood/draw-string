f4b9e3a1ff78c94e806229e2c5ccbac744ba0591
joshwood <josh.norwood@gmail.com>
2014-04-23 10:31:43 -0500

    added a canvas support check and a new route to show an error page if no support

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,6 +15,9 @@ app.config(['$routeProvider',
             when('/drawings/:drawingId', {
                 templateUrl: 'views/drawings/drawing.tpl.html'
             }).
+            when('/unsupported', {
+                templateUrl: 'unsupported.html'
+            }).
             otherwise({
                 templateUrl: 'views/welcome/welcome.tpl.html'
             });
@@ -23,15 +26,38 @@ app.config(['$routeProvider',
 /*
  * placeholder controller
  */
-app.controller('MainController', ['$scope', function($scope){
+app.controller('MainController', ['$scope', '$rootScope', '$log', '$location', function($scope, $rootScope, $log, $location){
 
     $scope.socket = io.connect("http://"+window.location.hostname+":3000");
+
+    /*
+     * every time we change we'll check in case someone clicks the navs
+     */
+    $rootScope.$on("$locationChangeStart", function(event, next, current){
+        if(!isCanvasSupported()){
+            $log.info("routing to unsupported");
+            $location.url('/unsupported');
+        }
+    });
 
 }]);
 
 
-//Ensures there will be no 'console is undefined' errors (IE)
+/**
+ * Ensures there will be no 'console is undefined' errors (IE)\
+ * TODO actually we can use $log from angular
+ */
 window.console = window.console || (function(){
     var c = {}; c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function(){};
     return c;
 })();
+
+/**
+ * utility function to validate we have canvas support.
+ * TODO may eventually add modernizr
+ * @returns {boolean}
+ */
+function isCanvasSupported(){
+    var elem = document.createElement('canvas');
+    return !!(elem.getContext && elem.getContext('2d'));
+}
